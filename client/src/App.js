e0c5e565387a2a4b922cc96cd8a03e11c0570561
Steve A <arcticfulmar@posteo.net>
2025-12-12 09:28:16 +0000

    Hash based routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Overview from './components/Overview';
 import VerbConjugation from './components/VerbConjugation';
 import VerbPractice from './components/VerbPractice';
